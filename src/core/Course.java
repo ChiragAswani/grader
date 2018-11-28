643bcf8ffeab89d6881e01c4b3f50290598e4cf0
burstein <alanbur@bu.edu>
2018-11-28 13:03:27 -0500

    Minor changes

--- src/core/Course.java
+++ src/core/Course.java
@@ -1,4 +1,18 @@
 package core;
 
+import java.util.ArrayList;
+
 public class Course {
+    public ArrayList<Student> students;
+
+    public Course(){
+        this.students = new ArrayList<>();
+    }
+    public Course(ArrayList<Student> students){
+        this.students = students;
+    }
+
+    public void addStudent(){
+
+    }
 }
