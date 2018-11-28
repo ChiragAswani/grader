6d176e437cfc61a38036e276364b78ac7a500a27
Zhong Tu <zht@bu.edu>
2018-11-27 20:35:51 -0500

    db almost done

--- src/core/Course.java
+++ src/core/Course.java
@@ -1,4 +1,53 @@
 package core;
+import Student.Student;
+import grades.Gradable;
+
+import java.util.List;
+
+
 
 public class Course {
+    private List<Student> studentList;
+    private List<Gradable> gradableList;
+    private String courseName;
+    private String courseID;
+    public Course(){}
+    public Course(String ID,String name,List<Student> sList,List<Gradable> gList){
+        courseID=ID;
+        courseName=name;
+        studentList=sList;
+        gradableList=gList;
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
+
+    public List<Gradable> getGradableList() {
+        return gradableList;
+    }
+
+    public void setGradableList(List<Gradable> gradableList) {
+        this.gradableList = gradableList;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
 }
--- src/grades/Gradable.java
+++ src/grades/Gradable.java
@@ -1,14 +1,101 @@
 package grades;
 
-public class Gradable {
-    public String assignmentName;
-    public int gID;
-    public double maxScore;
-    public double receivedScore;
+import java.math.BigDecimal;
 
-    public Gradable (String assignmentName, double maxScore, double receivedScore){
+public class Gradable {
+    private String assignmentName;
+    private int gID;
+    private String courseID;
+    private BigDecimal maxScore;
+    private BigDecimal weight_ungrad;
+    private BigDecimal weight_grad;
+    public double receivedScore;
+    private int costomized;
+    private String type;
+    //public double receivedScore;
+    public Gradable(){}
+
+    public Gradable (String courseID,int gradableId, String assignmentName, BigDecimal maxScore, BigDecimal weightU, BigDecimal weightG, int c, String t){
+        this.courseID=courseID;
+        this.gID=gradableId;
         this.assignmentName = assignmentName;
         this.maxScore = maxScore;
+        this.weight_ungrad=weightU;
+        this.weight_grad=weightG;
+        this.costomized=c;
+        this.type=t;
+        //this.receivedScore = receivedScore;
+    }
+
+    public String getAssignmentName() {
+        return assignmentName;
+    }
+
+    public void setAssignmentName(String assignmentName) {
+        this.assignmentName = assignmentName;
+    }
+
+    public int getgID() {
+        return gID;
+    }
+
+    public void setgID(int gID) {
+        this.gID = gID;
+    }
+
+    public BigDecimal getMaxScore() {
+        return maxScore;
+    }
+
+    public void setMaxScore(BigDecimal maxScore) {
+        this.maxScore = maxScore;
+    }
+
+    public BigDecimal getWeight_ungrad() {
+        return weight_ungrad;
+    }
+
+    public void setWeight_ungrad(BigDecimal weight_ungrad) {
+        this.weight_ungrad = weight_ungrad;
+    }
+
+    public BigDecimal getWeight_grad() {
+        return weight_grad;
+    }
+
+    public void setWeight_grad(BigDecimal weight_grad) {
+        this.weight_grad = weight_grad;
+    }
+
+    public double getReceivedScore() {
+        return receivedScore;
+    }
+
+    public void setReceivedScore(double receivedScore) {
         this.receivedScore = receivedScore;
+    }
+
+    public int getCostomized() {
+        return costomized;
+    }
+
+    public void setCostomized(int costomized) {
+        this.costomized = costomized;
+    }
+
+    public String getCourseID() {
+        return courseID;
+    }
+
+    public void setCourseID(String courseID) {
+        this.courseID = courseID;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
     }
 }
