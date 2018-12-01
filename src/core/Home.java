58ba98fc3ce89552468599fc513b7c9d7cd5d7d3
burstein <alanbur@bu.edu>
2018-12-01 12:43:34 -0500

    home page wires

--- src/core/Home.java
+++ src/core/Home.java
@@ -1,14 +1,41 @@
 package core;
 
+import DAO.CourseDAO;
+import database.CourseDB;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Home {
     List<Course> courses;
 
-    public Boolean login(){
+    public Boolean login(String pass){
         // Try auth
-        // if usename and password match return true else false
+        // if username and password match return true else false
         return false;
+    }
+
+    public void changeLogin(String pass){
+        //
+    }
+    public List<Integer> seeCourses(){
+        //
+        CourseDAO cdb = new CourseDB();
+        try{
+            List<Course> courses = cdb.findAllCourse();
+            List<Integer> courseIds = courses
+                    .stream()
+                    .map(course ->  Integer.valueOf(course.getCourseID()))
+                    .collect(Collectors.toList());
+            return courseIds;
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+    public void loadCourse(){
+
     }
 
 }
