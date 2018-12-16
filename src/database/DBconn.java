9770d767ff676da0a999e0420180b637c711b38c
Zhong Tu <zht@bu.edu>
2018-12-16 13:55:58 -0500

    fix duplicated data

--- src/database/DBconn.java
+++ src/database/DBconn.java
@@ -6,10 +6,9 @@ import java.sql.*;
 
 public class DBconn {
     private static String driver="com.mysql.cj.jdbc.Driver";
-    private static String url="jdbc:mysql://127.0.0.1:3306/" +"?serverTimezone=UTC";
+    private static String url="jdbc:mysql://127.0.0.1:3306/oop_gradingysy" +"?serverTimezone=UTC";
     private static String user="root";
-  //  private static String password= CredentialConstants.dbPass;
-  private static String password= "1111";
+    private static String password= CredentialConstants.dbPass;
     static {
         try {
             Class.forName(driver);
